c4c31e96f1eff5392b274ada0539f1b4d6fac3c0
weironRX <dmitri.p.sft@gmail.com>
2024-04-28 15:01:03 +0300

    adaptive works

--- dist/static/chunks/webpack.js
+++ dist/static/chunks/webpack.js
@@ -153,7 +153,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "3908f1e2092a6209"; }
+/******/ 		__webpack_require__.h = function() { return "20ce5d6237860d47"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
